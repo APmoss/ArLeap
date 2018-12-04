20808f65a307465e6d410648d4c898cca93d7a34
APmoss <apmoss.13@gmail.com>
2018-12-04 17:05:38 -0600

    Basic project settings

---
+++ Assets/Scripts/SpinFloat.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinFloat : MonoBehaviour {
+	private void Update () {
+		transform.Rotate(0f, 30f * Time.deltaTime, 0f);
+		transform.localPosition = new Vector3(0f, (Mathf.Sin(Time.time) + 1) / 6, 0f);
+
+		Debug.Log($"wew {Time.deltaTime}");
+	}
+}
