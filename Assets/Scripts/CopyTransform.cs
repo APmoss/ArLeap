20b8822295c50642c40aba95146c5532a4f55f90
APmoss <apmoss.13@gmail.com>
2018-12-16 22:05:07 -0600

    ManipHands and reorganization

---
+++ Assets/Scripts/CopyTransform.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CopyTransform : MonoBehaviour {
+	public Transform Target;
+
+	void Update () {
+		transform.localPosition = Target.localPosition;
+		transform.localRotation = Target.localRotation;
+		transform.localScale = Target.localScale;
+	}
+}
--- Assets/Scripts/NetworkSpinFloat.cs
+++ Assets/Scripts/NetworkSpinFloat.cs
@@ -3,7 +3,7 @@ using UnityEngine.Networking;
 
 public class NetworkSpinFloat : NetworkBehaviour {
 	private void Update() {
-		if (hasAuthority) {
+		if (isServer) {
 			transform.Rotate(0f, 30f * Time.deltaTime, 0f);
 			transform.localPosition = new Vector3(0f, (Mathf.Sin(Time.time) + 1) / 6, 0f);
 		}
